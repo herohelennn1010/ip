cdd96d4ec6f0c5ac0f39d6eb01757fa06ae65a23
He Jingqi <helen07070409@gmail.com>
2026-08-21 00:36:50 +0800

    Add simple greeting and exit messages

--- src/main/java/Sophon.java
+++ src/main/java/Sophon.java
@@ -8,12 +8,22 @@ public class Sophon {
      * @param args command line arguments, currently unused
      */
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = " ____              _                 \n"
-                + "/ ___|  ___  _ __ | |__   ___  _ __  \n"
-                + "\\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\ \n"
+                + "/ ___|  ___  _ __ | |__   ___  _ __ \n"
+                + "\\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\\n"
                 + " ___) | (_) | |_) | | | | (_) | | | |\n"
                 + "|____/ \\___/| .__/|_| |_|\\___/|_| |_|\n"
                 + "            |_|                       \n";
-        System.out.println(banner);
+        String greeting = "你好! I'm Sophon.\n"
+                + "I'm listening.";
+        String bye = "Our conversation ends here.\n"
+                + "Until we meet again.";
+        System.out.println(line);
+        System.out.print(banner);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
     }
 }
