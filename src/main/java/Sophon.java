bcb88f2fc740bddcb1f725e809626453d8da47a6
He Jingqi <helen07070409@gmail.com>
2026-08-21 00:22:50 +0800

    Rename chatbot to Sophon

---
+++ src/main/java/Sophon.java
@@ -0,0 +1,19 @@
+/**
+ * Entry point for the Sophon chatbot.
+ */
+public class Sophon {
+    /**
+     * Prints the chatbot banner.
+     *
+     * @param args command line arguments, currently unused
+     */
+    public static void main(String[] args) {
+        String banner = " ____              _                 \n"
+                + "/ ___|  ___  _ __ | |__   ___  _ __  \n"
+                + "\\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\ \n"
+                + " ___) | (_) | |_) | | | | (_) | | | |\n"
+                + "|____/ \\___/| .__/|_| |_|\\___/|_| |_|\n"
+                + "            |_|                       \n";
+        System.out.println(banner);
+    }
+}
