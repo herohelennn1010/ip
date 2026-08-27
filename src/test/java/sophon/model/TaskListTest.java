72d947f4317b8e513ee9ba498f3c4117a6a7d5f4
He Jingqi <helen07070409@gmail.com>
2026-08-28 02:56:26 +0800

    Add support for JUnit testing for two methods

---
+++ src/test/java/sophon/model/TaskListTest.java
@@ -0,0 +1,13 @@
+package sophon.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals; ...
+
+public class TaskList {
+    @Test
+    public void isValidIndex_emptyList_returnsFalse() {
+        TaskList empty = new TaskList();
+        assertEquals(false, )
+    }
+}
