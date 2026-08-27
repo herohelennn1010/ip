5fde35d80e9336b678b78fdc848ad61c5fe74626
He Jingqi <helen07070409@gmail.com>
2026-08-28 03:01:33 +0800

    Add further support JUnit testing

---
+++ src/test/java/sophon/logic/ParserTest.java
@@ -0,0 +1,33 @@
+package sophon.logic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import sophon.exception.SophonException;
+
+public class ParserTest {
+    @Test
+    public void parseTaskIndex_validTaskNumbers_returnsZeroBasedIndex() throws SophonException {
+        assertEquals(0, Parser.parseTaskIndex("mark 1", "mark", "Missing task number."));
+        assertEquals(9, Parser.parseTaskIndex("mark 10", "mark", "Missing task number."));
+        assertEquals(2, Parser.parseTaskIndex("mark     3     ", "mark", "Missing task number."));
+    }
+
+    @Test
+    public void parseTaskIndex_missingTaskNumber_throwsExceptionWithGivenMessage() {
+        SophonException exception = assertThrows(SophonException.class,
+                () -> Parser.parseTaskIndex("mark", "mark", "Missing task number."));
+
+        assertEquals("Missing task number.", exception.getMessage());
+    }
+
+    @Test
+    public void parseTaskIndex_nonNumericTaskNumber_throwsException() {
+        SophonException exception = assertThrows(SophonException.class,
+                () -> Parser.parseTaskIndex("mark one", "mark", "Missing task number."));
+
+        assertEquals("Task numbers must be written as numerals.", exception.getMessage());
+    }
+}
--- src/test/java/sophon/model/TaskListTest.java
+++ src/test/java/sophon/model/TaskListTest.java
@@ -1,13 +1,26 @@
 package sophon.model;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals; ...
-
-public class TaskList {
+public class TaskListTest {
     @Test
     public void isValidIndex_emptyList_returnsFalse() {
         TaskList empty = new TaskList();
-        assertEquals(false, )
+        assertFalse(empty.isValidIndex(0));
+        assertFalse(empty.isValidIndex(1));
+        assertFalse(empty.isValidIndex(-1));
+    }
+
+    @Test
+    public void isValidIndex_indexWithinList_returnsTrue() {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("test1"));
+        tasks.add(new Todo("test2"));
+
+        assertTrue(tasks.isValidIndex(0));
+        assertTrue(tasks.isValidIndex(1));
     }
 }
